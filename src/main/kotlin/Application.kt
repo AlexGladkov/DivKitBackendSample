51dce4d710d6e45280ff98d903bf6ad530c9d8ab
Mobile Developer <bob298@yandex.ru>
2025-02-14 12:07:14 +0700

    sample done

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,13 +4,10 @@ import io.ktor.server.netty.Netty
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.cors.routing.*
-import kotlinx.serialization.SerialName
 
 fun main() {
     embeddedServer(Netty, port = 9090, host = "0.0.0.0") {
@@ -27,7 +24,7 @@ fun main() {
         }
         routing {
             get("/") {
-                call.respond(SecondSample.generateResponse())
+                call.respond(FirstSample.generateResponse())
             }
         }
     }.start(wait = true)
